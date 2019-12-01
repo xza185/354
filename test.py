aaaa73355f3b5c6970725ae9f88520a3f181c050
xza185 <xza185@sfu.ca>
2019-12-01 02:07:03 -0800

    t

--- test.py
+++ test.py
@@ -65,7 +65,7 @@ while True:
         row = cur.fetchone()
         while row:
             #print (("SQL Server standard login name= %s") %  (row[0]))
-            df=df.append({'id':row[0],'name':row[1],'description':row[2],'number_of_bedrooms':row[3], 'price':row[4]})
+            df=df.append({'id':row[0],'name':row[1],'description':row[2],'number_of_bedrooms':row[3], 'price':row[4]},ignore_index=True)
             # from Python version 3: print is a function, not a statement.
             row = cur.fetchone()
         conn.close()
