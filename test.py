16b908eb708f3b6feea61d8e686d9e798b5a91ec
xza185 <xza185@sfu.ca>
2019-12-01 01:34:03 -0800

    t

--- test.py
+++ test.py
@@ -10,48 +10,48 @@ def first_25(d):
 
 while True:
     print("Search Listings type s; Book Listing type b; Write Review type w; Quit type q")
-    t = raw_input (">")
+    t = input(">")
     if t=='q':
         break 
     if t=='s':
-        min = raw_input("please enter minimum price:")
+        min = input("please enter minimum price:")
         while True:
             try:
                 min=float(min)
                 break
             except ValueError: 
-                min = raw_input("Not a float, please enter minimum price:")
+                min = input("Not a float, please enter minimum price:")
 
-        max = raw_input("please enter maximum price:")
+        max = input("please enter maximum price:")
         while True:
             try:
                 max=float(max)
                 break
             except ValueError: 
-                max = raw_input("Not a float, please enter maximum price:")
-        bed_room=raw_input("please enter number of bedrooms:")
+                max = input("Not a float, please enter maximum price:")
+        bed_room=input("please enter number of bedrooms:")
         bed_room=unicode(bed_room,'utf-8')
         while ~bed_room.isnumeric():
             try:
                 bed_room=int(bed_room)
                 break
             except ValueError: 
-                bed_room = raw_input("Not a integer, please enter number of bedrooms:")
+                bed_room = input("Not a integer, please enter number of bedrooms:")
                 bed_room=unicode(bed_room,'utf-8')
-        start=raw_input("please enter start date:")
+        start=input("please enter start date:")
         while True:
             try:
                 start=pd.to_datetime(start)
                 break
             except ValueError: 
-                start = raw_input("Not a valide date, please enter start date:")
-        end=raw_input("please enter end date:")
+                start = input("Not a valide date, please enter start date:")
+        end=input("please enter end date:")
         while True:
             try:
                 end=pd.to_datetime(end)
                 break
             except ValueError: 
-                end = raw_input("Not a valide date, please enter end date:")
+                end = input("Not a valide date, please enter end date:")
         ##
         conn = pymssql.connect(host='cypress.csil.sfu.ca', user='s_xza185', password='JT3rG3HthGtMbg3A', database='xza185354')
         cur = conn.cursor()
